98e021d157f991fa5bac9509386f331126f9a4ac
A I <atbisrael@gmail.com>
2017-12-24 03:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function inc(n) {
 function dec(n){
   return n-1;
 }
+<<<<<<< HEAD
 function makeInt(a){
   if (isNaN(a)){
     var message = "Not an Integer";
@@ -30,5 +31,13 @@ return int;
 }
 function preserveDecimal(a){
  var num = parseFloat(a);
+=======
+function makeInt(n){
+return parseInt('n',10);
+
+}
+function preserveDecimal(n){
+ var num = parseFloat(n);
+>>>>>>> c913dec406c06c341de1821137e5cdf5da5cc6c6
   return num;
 }
--- test/index-test.js
+++ test/index-test.js
@@ -11,7 +11,7 @@ it('add(a, b) adds two numbers and returns the result', function() {
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
   expect(subtract(a, b)).toEqual(a - b)
-})
+}) 
 
 it('multiply(a, b) multiplies two numbers and returns the result', function() {
   expect(multiply(a, b)).toEqual(a * b)
